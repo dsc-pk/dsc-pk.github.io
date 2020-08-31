4891f29d1b4089e2875489320712eac8cd2febd9
saadhassan <saad.haxxan786@gmail.com>
2020-08-31 10:02:42 +0500

    files upload

---
+++ precache-manifest.ec3b139c1807788882d89b02ad9ad2b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7581842f2849bd588aac4813422b8bd2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7050619098751b3da342",
+    "url": "./static/css/main.ccc6d5c9.chunk.css"
+  },
+  {
+    "revision": "33fcaf5e52706601fdb1",
+    "url": "./static/js/2.43c7fe45.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "./static/js/2.43c7fe45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7050619098751b3da342",
+    "url": "./static/js/main.e1323dda.chunk.js"
+  },
+  {
+    "revision": "9cdaea01d77998eec431",
+    "url": "./static/js/runtime-main.074ec971.js"
+  },
+  {
+    "revision": "cc949774e2b8b0ac9baad375c2f98944",
+    "url": "./static/media/dsclogo.cc949774.png"
+  },
+  {
+    "revision": "8fb0d5d99c4d1a2c2ea850290ece36e4",
+    "url": "./static/media/shortlogo.8fb0d5d9.png"
+  }
+]);
